4677c2a81c1098de8009cae81fa9cc51b357017e
Bill Bruyn <bill.bruyn@gmail.com>
2014-01-24 04:30:01 -0700

    Add missing deployment authentication step

--- src/main/java/com/isomorphic/maven/mojo/DeployMojo.java
+++ src/main/java/com/isomorphic/maven/mojo/DeployMojo.java
@@ -19,16 +19,25 @@ package com.isomorphic.maven.mojo;
  * under the License.
  */
 
+import java.util.List;
 import java.util.Set;
 
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.settings.Server;
+import org.apache.maven.settings.crypto.DefaultSettingsDecryptionRequest;
+import org.apache.maven.settings.crypto.SettingsDecrypter;
+import org.apache.maven.settings.crypto.SettingsDecryptionRequest;
+import org.apache.maven.settings.crypto.SettingsDecryptionResult;
 import org.eclipse.aether.artifact.Artifact;
 import org.eclipse.aether.deployment.DeployRequest;
 import org.eclipse.aether.deployment.DeploymentException;
+import org.eclipse.aether.repository.Authentication;
 import org.eclipse.aether.repository.RemoteRepository;
+import org.eclipse.aether.util.repository.AuthenticationBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -74,25 +83,35 @@ public final class DeployMojo extends AbstractPackagerMojo {
 	@Parameter(property="repositoryType", defaultValue="default")
 	private String repositoryType;
 	
+	@Component 
+    private SettingsDecrypter settingsDecrypter; 
+	
 	/**
 	 * Deploy each of the provided {@link Module}s, along with their SubArtifacts (POMs, JavaDoc bundle, etc.), to the repository location
 	 * indicated by {@link #repositoryUrl}.
 	 */
 	@Override 
 	public void doExecute(Set<Module> artifacts) throws MojoExecutionException, MojoFailureException {
-	
+
+    	RemoteRepository.Builder builder = new RemoteRepository.Builder(repositoryId, repositoryType, repositoryUrl);
+		Authentication authentication = getAuthentication(repositoryId);
+    	builder.setAuthentication(authentication);
+
+    	LOGGER.info("Deploying to repositoryId '{}' with credentials: {}", repositoryId, authentication.toString());
+    	
+    	RemoteRepository repository = builder.build();
+		
 		for (Module artifact : artifacts) {
 	     
 			DeployRequest deployRequest = new DeployRequest();
-			deployRequest.addArtifact(artifact);
-	        
-	        for (Artifact subArtifact : artifact.getAttachments()) {
+			deployRequest.addArtifact(artifact); 
+        	deployRequest.setRepository(repository);
+        	
+			for (Artifact subArtifact : artifact.getAttachments()) {
 	        	deployRequest.addArtifact(subArtifact);
 	        }
-
+	        
 	        try {
-	        	RemoteRepository repository = new RemoteRepository.Builder(repositoryId, repositoryType, repositoryUrl).build();
-	        	deployRequest.setRepository(repository);
 				repositorySystem.deploy(repositorySystemSession, deployRequest);
 			} catch (DeploymentException e) {
 				throw new MojoFailureException("Deployment failed: ", e);
@@ -100,4 +119,51 @@ public final class DeployMojo extends AbstractPackagerMojo {
 
 		}		
 	}
+	
+	/**
+	 * Returns user credentials for the server with the given id, as kept in Maven
+	 * settings.  
+	 * 
+	 * @param serverId the id of the server containing the authentication credentials
+	 * @return the Authentication credentials for the given server with the given id
+	 * 
+	 * @see http://maven.apache.org/settings.html#Servers
+	 */
+	private Authentication getAuthentication(String serverId) {
+		
+		Authentication authentication = null;
+		Server server = getDecryptedServer(serverId);
+		
+		if (server != null) {
+	    	authentication = new AuthenticationBuilder()
+				.addUsername(server.getUsername())
+				.addPassword(server.getPassword())
+				.addPrivateKey(server.getPrivateKey(), server.getPassphrase())
+				.build();
+		}
+		return authentication;
+	}
+
+	/**
+	 * Decrypt settings and return the server element with the given id.  Useful for e.g., reading encrypted 
+	 * user credentials.
+	 * 
+	 * @param id the id of the server to be decrypted
+	 * @return a Server with its protected elements decrypted, if one is found with the given id.  Null otherwise.
+	 * 
+	 * @see http://maven.apache.org/guides/mini/guide-encryption.html
+	 */
+    private Server getDecryptedServer(String id) { 
+        final SettingsDecryptionRequest settingsDecryptionRequest = new DefaultSettingsDecryptionRequest(); 
+        settingsDecryptionRequest.setServers(settings.getServers()); 
+        final SettingsDecryptionResult decrypt = settingsDecrypter.decrypt(settingsDecryptionRequest); 
+        List<Server> servers = decrypt.getServers();
+        
+        for (Server server : servers) {
+        	if (server.getId().equals(id)) {
+        		return server;
+        	}
+        }
+        return null;
+    } 
 }
